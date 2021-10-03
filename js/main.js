c6285dea4e1075edb9b03c89684a1a749a252712
Andrew <korotkov915@gmail.com>
2021-10-03 18:25:18 +0300

    FIX: replace cod from branch tast-module2 to branch module4-task1

--- js/main.js
+++ js/main.js
@@ -13,8 +13,6 @@ const randomInt = function (from, to) {
   const resultInt = Math.floor(Math.random() * (to - from + 1) + from);
   console.log('Результат: '+ resultInt);
 };
-// eslint-disable-next-line no-console
-randomInt(40, 420);
 
 const randomFloat = function (from, to, range) {
   if (from < 0 || to < 0) {
@@ -27,11 +25,10 @@ const randomFloat = function (from, to, range) {
     console.log('Предупреждение: для разнообразия результата выберайте разные значения минимального и максимального диапазона. Сейчас результат будет: ' + from);
     return console.log('Результат: '+ from);
   }
-  const convertNumber = Math.pow(10, range); //10 возводим в степень range
-  const One = (from * convertNumber).toFixed(0);
-  const Two = (to * convertNumber).toFixed(0);
-  const result = (Math.floor(Math.random() * (Two - One + 1) + +One))/convertNumber;
+  const result = Number((Math.random() * (to - from ) + from).toFixed(range));
   return console.log('Результат: '+ result);
 };
 
-randomFloat(4.12, 4.92, 2);
+// eslint-disable-next-line no-console
+randomInt(40, 420);
+randomFloat(4.5, 495.92, 2);
