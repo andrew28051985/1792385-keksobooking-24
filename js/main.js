6bc54a64b59d3b9d178ec5b346150ee32962b973
Andrew <korotkov915@gmail.com>
2021-10-09 11:57:38 +0300

    ADD: create modul random-number.js and connect in main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-const getRandomPositiveInt = (from, to) => {
-  const lower = Math.ceil(Math.min(Math.abs(from), Math.abs(to)));
-  const upper = Math.floor(Math.max(Math.abs(from), Math.abs(to)));
-  const resultInt = Math.floor(Math.random() * (upper - lower + 1) + lower);
-  return resultInt;
-};
-const getRandomPositiveFloat = (from, to, range) => {
-  const lower = Math.min(Math.abs(from), Math.abs(to));
-  const upper = Math.max(Math.abs(from), Math.abs(to));
-  const resultFloat = Math.random() * (upper - lower ) + lower;
-  return resultFloat.toFixed(range);
-};
+import {getRandomPositiveInt, getRandomPositiveFloat} from './random-number.js';
 
 const MAX_NUMBER_AUTOR = 10;
 const MIN_LAT = 35.65000;
