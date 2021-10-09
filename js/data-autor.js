2d82bb6a64ea30d26ec9121aa8f72a3ba3e301ab
Andrew <korotkov915@gmail.com>
2021-10-09 14:01:35 +0300

    FIX: remaded concatination in data-autor.js

--- js/data-autor.js
+++ js/data-autor.js
@@ -5,12 +5,10 @@ const MAX_NUMBER_AUTOR = 10;
 const createAutor = () => {
   let numberAutor = getRandomPositiveInt(1, MAX_NUMBER_AUTOR);
   if (String(numberAutor).length === 1) {
-    // eslint-disable-next-line prefer-template
-    numberAutor = '0' + numberAutor;
+    numberAutor = `0${numberAutor}`;
   }
   return {
-    // eslint-disable-next-line prefer-template
-    avatar: 'img/avatars/user' + numberAutor + '.png',
+    avatar: `img/avatars/user${numberAutor}.png`,
   };
 };
 
