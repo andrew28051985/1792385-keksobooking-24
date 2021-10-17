ae31d518cc884b8dcfbb784dc772ac2358b1a21b
Andrew <korotkov915@gmail.com>
2021-10-17 13:48:04 +0300

    ADD: finished create cards

--- js/main.js
+++ js/main.js
@@ -1,10 +1,2 @@
-import {createAds} from './util.js';
-import {addCard} from './card.js';
-
-const SIMILAR_ADS_COUNT = 10;
-const similarAds = Array.from({length: SIMILAR_ADS_COUNT}, createAds);
-
-// eslint-disable-next-line no-console
-console.log(similarAds);
-// eslint-disable-next-line no-console
-console.log(addCard);
+import './util.js';
+import './card.js';
--- js/util.js
+++ js/util.js
@@ -1,12 +1,16 @@
 import {createAutor, createLocation, createOffer} from './data.js';
 
+const SIMILAR_ADS_COUNT = 1;
+
 const createAds = () => {
-  const AUTOR = createAutor();
-  return {
-    autor: AUTOR,
+  const ads = {
+    autor: createAutor(),
     offer: createOffer(),
     location: createLocation(),
   };
+  return ads;
 };
 
-export {createAds};
+const similarAds = () => Array.from({length: SIMILAR_ADS_COUNT}, createAds);
+
+export {createAds, similarAds};
