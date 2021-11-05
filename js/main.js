ebd0a616fc4c840a711921472d1447e56c7f073c
Andrew <korotkov915@gmail.com>
2021-11-05 13:42:58 +0300

    ADD: create enter 10 ads on map

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ import './form.js';
 import './map.js';
 import {createAd} from './map.js';
 
+const ADS_COUNT = 10;
+
 fetch('https://24.javascript.pages.academy/keksobooking/data')
   .then((response) => response.json())
   .then((ads) => {
-    createAd(ads);
+    createAd(ads.slice(0, ADS_COUNT));
   });
